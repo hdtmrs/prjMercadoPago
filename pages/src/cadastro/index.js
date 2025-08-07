01fc3fb3b924315f63fb161d30e06b7d5257df07
hdtmrs <hdtmrsame8@gmail.com>
2025-08-07 08:00:58 -0300

    :tada: Inicializando projeto

---
+++ pages/src/cadastro/index.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+import {View, Text} from 'react-native';
+
+const CadastroScreen = () => {
+    return(
+        <View>
+            <Text>
+                Esta na tela certa
+            </Text>
+        </View>
+    );
+};
+export default CadastroScreen;

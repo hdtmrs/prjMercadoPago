9179b1ef1c580181a192979d522784586fb3ea8d
unknown <hdtmrsame8@gmail.com>
2025-08-08 14:18:47 -0300

    feat:Adicionando rotina de inserção

--- mercadoPagoWeb/app/Http/Controllers/UsuarioController.php
+++ mercadoPagoWeb/app/Http/Controllers/UsuarioController.php
@@ -4,8 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Usuario;
 
 class UsuarioController extends Controller
 {
-    //
+    public function store(Request $request) {
+        $dados = $request->validate([
+            'nome' => 'required|string|max:100',
+            'email' => 'required|email|max:225',
+            'senha' => 'required|string|min:8',
+            'cpf' => 'required|string|min:11',
+        ]);
+
+        $usuario = Usuario::create($dados);
+
+        return response()->json($usuario, 201);
+    }
 }
--- mercadoPagoWeb/app/Models/Usuario.php
+++ mercadoPagoWeb/app/Models/Usuario.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Usuario extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'email',
+        'senha',
+        'cpf',
+    ];
+
+    protected $table = 'tbUsuario';
+
+    public $timestamps = true;
+
+    protected $hidden = [
+        'senha',
+    ];
+
+    protected $casts = [
+        'nome' => 'string',
+        'email' => 'string',
+        'senha' => 'string',
+        'cpf' => 'string',
+    ];
+
+
 }

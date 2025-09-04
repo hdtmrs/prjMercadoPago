dbb5190414274550e3ccc4badf71a576015aad2b
hdtmrs <hdtmrsame8@gmail.com>
2025-09-04 12:11:30 -0300

    feat:Adicionando inicio da home

--- mercadoPagoMobile/pages/src/home/index.js
+++ mercadoPagoMobile/pages/src/home/index.js
@@ -1,10 +1,37 @@
-import React from "react";
-import {View, Text} from 'react-native';
+import React, {useState, useEffect} from "react";
+import {View, Text, Pressable} from 'react-native';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}) => {
+    const [nome, setNome] = useState('');
+    useEffect(() => {
+        const buscarDados = async () => {
+            const tokenUser = await AsyncStorage.getItem('userToken');
+            const response = await axios.get('http://127.0.0.1:8000/api/getdate', {}, {
+                Headers: {
+                    Authenticate: `Bearer: ${tokenUser}`
+                }
+            })
+        }
+    },[]);
     return(
         <View>
-            <Text>Tela Home</Text>
+            
+            <View>
+                <Pressable>
+                    <Text>Nome</Text>
+                </Pressable>
+                <Pressable>
+                    <Text>Notificação</Text>
+                </Pressable>
+                <Pressable>
+                    <Text>Ajuda</Text>
+                </Pressable>
+            </View>
+            <View>
+                
+            </View>
         </View>
     );
 };
